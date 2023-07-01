b0501e4291f9f0c1b5485401a023a5874dff5695
shubhang <Shubhangd205@gmail.com>
2023-07-01 10:47:14 +0530

    commit 2

---
+++ app/src/main/java/com/example/notesapp/screen/NoteScreen.kt
@@ -0,0 +1,15 @@
+package com.example.notesapp.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NoteScreen(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteScreenPreview(){
+    NoteScreen()
+}
